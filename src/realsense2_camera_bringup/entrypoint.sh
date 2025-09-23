825b0007870712be37d18e43bc61fa8e0de16480
victor <victorbsfm@gmail.com>
2025-09-23 08:10:35 -0300

    Update .gitmodules

--- src/realsense2_camera_bringup/entrypoint.sh
+++ src/realsense2_camera_bringup/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -e
 
-# Source ROS 2 environment
+# Source ROS 2
 source /opt/ros/jazzy/setup.bash
 
-# Source workspace setup
+# Source workspace
 if [ -f "/home/ros/ros2_ws/install/setup.bash" ]; then
     source /home/ros/ros2_ws/install/setup.bash
 fi
 
+# Substitui o processo por ros2 launch
 exec ros2 launch realsense2_camera rs_launch.py
