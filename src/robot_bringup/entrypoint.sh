403d700da44ddc8d06b00eb8a049ea79045a7832
victor <victorbsfm@gmail.com>
2025-10-09 15:02:00 -0300

    Add namespace compatibility

--- src/robot_bringup/entrypoint.sh
+++ src/robot_bringup/entrypoint.sh
@@ -9,11 +9,4 @@ if [ -f "/home/ros/ros2_ws/install/setup.bash" ]; then
     source /home/ros/ros2_ws/install/setup.bash
 fi
 
-# Executa o launcher em segundo plano
-ros2 launch tf_launcher static_tf.launch.py &
-
-# Executa o script Python em segundo plano
-python3 /home/ros/ros2_ws/src/scripts/dynamic_livox_tf.py &
-
-# Aguarda todos os processos em segundo plano terminarem
-wait
+python3 /home/ros/ros2_ws/src/scripts/dynamic_livox_tf.py --ros-args -r __ns:=$NAMESPACE 
