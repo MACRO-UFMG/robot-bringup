7286011ec3c09ba76c838038054f965ba98b2b4c
UFMG-PetroOP-1319 <ufmgpetrop1319@gmail.com>
2025-10-20 16:43:15 -0300

    preparing to merge files

--- src/scout_bringup/entrypoint.sh
+++ src/scout_bringup/entrypoint.sh
@@ -9,5 +9,19 @@ if [ -f "/home/ros/ros2_ws/install/setup.bash" ]; then
     source /home/ros/ros2_ws/install/setup.bash
 fi
 
+# Verificar se can0 já existe e está ativa
+if ip link show can0 &> /dev/null; then
+    
+    # Tentar derrubar a interface existente primeiro
+    sudo ip link set can0 down || true
+    sudo ip link delete can0 || true
+    
+    # Pequena pausa para garantir que a interface foi removida
+    sleep 1
+fi
+
+# Configurar nova interface CAN
+sudo ip link set can0 up type can bitrate 500000
+
 # Substitui o processo por ros2 launch
-ros2 launch scout_base scout_base.launch.py
+exec ros2 launch scout_base scout_mini_base.launch.py
